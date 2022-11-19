eeff80071d4c3b1befd49b5862a610040be20c0d
Emmanoel Dantas <emmanuel2011.07@gmail.com>
2022-11-19 17:54:18 -0300

    refactor - vaidation create user

--- src/services/createUser/CreateUserService.ts
+++ src/services/createUser/CreateUserService.ts
@@ -1,11 +1,14 @@
 import { User } from "../../entities/User";
 import { hash } from "bcryptjs";
 import { IUserRepository} from "../../repositories/IUserRepositories";
+import z, { discriminatedUnion } from "zod"
 
 interface CreateUserRequest {
     username: String,
     password: String
 }
+
+const  PASSWORD_VALIDATION_REGEX  = /((?=.*\d))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).{8,}$/;
 
 export class CreateUserService {
     constructor(private userRepository: IUserRepository){}
@@ -15,6 +18,15 @@ export class CreateUserService {
         
         if(userAreadyExists){
             throw new Error("User Already Exists!!")
+        }
+
+        const data = z.object({
+            username: z.string().min(3),
+            password: z.string().min(8).regex(PASSWORD_VALIDATION_REGEX)
+        })
+
+        if(!data){
+            throw new Error("Username/Password Invalid ")
         }
 
         const passwordHash = await hash(String(password), 8)
